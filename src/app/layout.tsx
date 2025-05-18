678a3ec48418625595f1f05c3ab2a1d2c38317c3
katy-paola <kbarbozab@unicartagena.edu.co>
2025-05-18 07:33:18 -0500

    Refactor layout component: update font imports and clean up metadata formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 import {
   ClerkProvider,
   SignInButton,
@@ -6,37 +6,35 @@ import {
   SignedIn,
   SignedOut,
   UserButton,
-} from '@clerk/nextjs'
-import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+} from "@clerk/nextjs";
+import { DM_Sans } from "next/font/google";
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
+const dmSans = DM_Sans({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'BooK - No pierdas tus libros por leer',
-  description: 'BooK te ayuda a no perder de vista tus libros pendientes. Guarda, organiza y accede a tus recomendaciones favoritas en tu biblioteca digital personal.',
+  title: "BooK - No pierdas tus libros por leer",
+  description:
+    "BooK te ayuda a no perder de vista tus libros pendientes. Guarda, organiza y accede a tus recomendaciones favoritas en tu biblioteca digital personal.",
   icons: {
     icon: "/favicon.svg",
-  }
-}
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body className={`${dmSans.className}`}>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
               <SignInButton />
@@ -50,5 +48,5 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
