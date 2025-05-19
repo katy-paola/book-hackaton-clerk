87d2a729d05c2e1d8cd7d7e3af25cf014cef6b3c
katy-paola <kbarbozab@unicartagena.edu.co>
2025-05-18 22:05:45 -0500

    Add ResetPassword component and integrate it into SignInForm for password recovery

---
+++ src/app/auth/components/ResetPassword.tsx
@@ -0,0 +1,143 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useAuth, useSignIn } from "@clerk/nextjs";
+import type { NextPage } from "next";
+import { redirect, useRouter } from "next/navigation";
+
+const ForgotPasswordPage: NextPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [code, setCode] = useState("");
+  const [successfulCreation, setSuccessfulCreation] = useState(false);
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+  const { isSignedIn } = useAuth();
+  const { isLoaded, signIn, setActive } = useSignIn();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push("/");
+    }
+  }, [isSignedIn, router]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  // Send the password reset code to the user's email
+  async function create(e: React.FormEvent) {
+    e.preventDefault();
+    await signIn
+      ?.create({
+        strategy: "reset_password_email_code",
+        identifier: email,
+      })
+      .then(() => {
+        setSuccessfulCreation(true);
+        setError("");
+      })
+      .catch((err) => {
+        console.error("error", err.errors[0].longMessage);
+        setError(err.errors[0].longMessage);
+      });
+  }
+
+  // Reset the user's password.
+  // Upon successful reset, the user will be
+  // signed in and redirected to the home page
+  async function reset(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError("Las contraseñas no coinciden");
+      return;
+    }
+    await signIn
+      ?.attemptFirstFactor({
+        strategy: "reset_password_email_code",
+        code,
+        password,
+      })
+      .then((result) => {
+        if (result.status === "complete") {
+          // Set the active session to
+          // the newly created session (user is now signed in)
+          setActive({ session: result.createdSessionId });
+          setError("");
+          redirect("/auth");
+        } else {
+          console.log(result);
+        }
+      })
+      .catch((err) => {
+        console.error("error", err.errors[0].longMessage);
+        setError(err.errors[0].longMessage);
+      });
+  }
+
+  return (
+    <form onSubmit={!successfulCreation ? create : reset}>
+      <fieldset>
+        <legend>¿Olvidaste tu contraseña</legend>
+        <p>Puedes restablecerla aquí</p>
+        {!successfulCreation && (
+          <>
+            <label htmlFor="email">
+              Correo
+              <input
+                type="email"
+                placeholder="ejemplo@correo.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <div>
+              <button>Enviar código</button>
+              <button type="button">Cancelar</button>
+            </div>
+            {error && <p>{error}</p>}
+          </>
+        )}
+
+        {successfulCreation && (
+          <>
+            <label htmlFor="password">
+              Nueva contraseña
+              <input
+                type="password"
+                value={password}
+                placeholder="••••••••"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <label htmlFor="confirm-password">
+              Nueva contraseña
+              <input
+                type="password"
+                value={confirmPassword}
+                placeholder="••••••••"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </label>
+            <label htmlFor="code">
+              Código de verificación
+              <input
+                type="code"
+                value={code}
+                placeholder="123456"
+                onChange={(e) => setCode(e.target.value)}
+              />
+            </label>
+            <button>Restablecer contraseña</button>
+            {error && <p>{error}</p>}
+          </>
+        )}
+      </fieldset>
+    </form>
+  );
+};
+
+export default ForgotPasswordPage;
---
+++ src/app/auth/components/SignInForm.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import * as React from "react";
+import { useSignIn } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import ResetPassword from "./ResetPassword";
+
+interface ClerkError {
+  errors?: { message: string }[];
+}
+
+export default function SignInForm() {
+  const { isLoaded, signIn, setActive } = useSignIn();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!isLoaded) return;
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: email,
+        password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.push("/");
+      } else {
+        console.error(
+          "Sign-in not complete:",
+          JSON.stringify(signInAttempt, null, 2)
+        );
+      }
+    } catch (err: unknown) {
+      if (err && typeof err === "object" && "errors" in err) {
+        const clerkErr = err as ClerkError;
+        setError(
+          clerkErr.errors?.[0]?.message || "Ocurrió un error al iniciar sesión."
+        );
+      } else {
+        setError("Error desconocido.");
+      }
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <div>
+            <label htmlFor="email">
+              Correo
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                id="email"
+                name="email"
+                type="email"
+                value={email}
+                placeholder="ejemplo@correo.com"
+              />
+            </label>
+            <p role="alert">El campo de correo no puede estar vacío</p>
+          </div>
+          <div>
+            <label htmlFor="password">
+              Contraseña
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                id="password"
+                name="password"
+                type="password"
+                value={password}
+                placeholder="••••••••"
+              />
+            </label>
+            <p role="alert">El campo de contraseña no puede estar vacío</p>
+            <Link href="/auth/reset-password">Olvidé mi contraseña</Link>
+          </div>
+          <button type="submit">Iniciar sesión</button>
+        </fieldset>
+        <button>
+          <Link href="/auth/sign-up">
+            ¿No tienes cuenta? <span>Regístrate</span>{" "}
+          </Link>
+        </button>
+      </form>
+      <ResetPassword />
+    </>
+  );
+}
