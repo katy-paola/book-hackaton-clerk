e6bd48572576913d2300903d3ed949a3468835e7
katy-paola <kbarbozab@unicartagena.edu.co>
2025-05-18 22:05:19 -0500

    Refactor AuthPage component to include SignUpForm

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,16 +1,22 @@
-import SignInForm from "./components/SignForm";
+import SignInForm from "./components/SignInForm";
+import SignUpForm from "./components/SignUpForm";
 
 export default async function AuthPage() {
   return (
     <div className="auth-page">
       <header>
-        <h1>Iniciar sesión en tu cuenta</h1>
+        <h1>
+          Iniciar sesión en tu cuenta
+          <br />
+          Crear una nueva cuenta
+        </h1>
         <div>
           <button>Inicia sesión</button>
           <button>Regístrate</button>
         </div>
       </header>
       <SignInForm />
+      <SignUpForm />
     </div>
   );
 }
