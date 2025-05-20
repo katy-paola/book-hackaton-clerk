6a28b0dd0e6672ea034951a40683aa37b47786d4
katy-paola <kbarbozab@unicartagena.edu.co>
2025-05-20 14:26:27 -0500

    Refactor BookDetailsPage component and styles for improved layout and user experience

---
+++ src/app/books/id-prueba/page.tsx
@@ -0,0 +1,56 @@
+import "../css/books.css";
+import Delete from "@/components/icons/Delete";
+import Edit from "@/components/icons/Edit";
+import Save from "@/components/icons/Save";
+import Link from "next/link";
+
+export default function BookDetailsPage() {
+  return (
+    <main className="book-details-container">
+      <header className="book-details-header">
+        <h1 className="book-details-title">Hábitos atómicos</h1>
+        <div className="book-details-info">
+          <p className="book-details-author">James Clear</p>
+          <small className="book-details-access-type">De pago</small>
+        </div>
+        <p className="book-details-category">
+          Libro de <span>Autoayuda</span>
+        </p>
+      </header>
+      <section className="book-details-content">
+        <img
+          className="book-details-cover"
+          src="/portada-prueba.png"
+          alt=""
+          width={343}
+          height={517}
+        />
+        <p className="book-details-description">
+          Este libro innovador nos revela exactamente cómo esos cambios
+          minúsculos pueden crecer hasta llegar a cambiar nuestra carrera
+          profesional, nuestras relaciones y todos los aspectos de nuestra vida.
+        </p>
+        <div className="book-details-ctas-container">
+          <Link className="book-details-main-cta" href="#">
+            Ir al libro
+          </Link>
+          <Link className="book-details-edit" href="#">
+            <Edit />
+          </Link>
+          <button className="book-details-delete">
+            <Delete />
+          </button>
+          <button className="book-details-save">
+            <Save />
+          </button>
+        </div>
+        <p className="book-details-user">
+          Publicado por{" "}
+          <a className="book-details-user-link" href="#">
+            Andrés Vizcaíno
+          </a>
+        </p>
+      </section>
+    </main>
+  );
+}
