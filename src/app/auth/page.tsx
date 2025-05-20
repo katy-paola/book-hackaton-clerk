4bd7f9d2aa3dbfb088ddc4c8b39b87484a90b16f
katy-paola <kbarbozab@unicartagena.edu.co>
2025-05-20 15:46:00 -0500

    Refactor AuthPage layout and styles for improved user experience

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,22 +1,23 @@
+import "./css/auth.css";
 import SignInForm from "./components/SignInForm";
 import SignUpForm from "./components/SignUpForm";
 
 export default async function AuthPage() {
   return (
-    <div className="auth-page">
-      <header>
-        <h1>
+    <section className="auth-page">
+      <header className="auth-header">
+        <h1 className="auth-title">
           Iniciar sesión en tu cuenta
           <br />
           Crear una nueva cuenta
         </h1>
-        <div>
-          <button>Inicia sesión</button>
-          <button>Regístrate</button>
+        <div className="auth-tabs">
+          <button className="auth-tab active-tab">Inicia sesión</button>
+          <button className="auth-tab">Regístrate</button>
         </div>
       </header>
       <SignInForm />
       <SignUpForm />
-    </div>
+    </section>
   );
 }
